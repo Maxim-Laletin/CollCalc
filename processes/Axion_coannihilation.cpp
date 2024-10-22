15c21cb5a899588083a3dce45d05a54e8256bde7
Maxim Laletin <maxim.n.laletin@gmail.com>
2024-10-23 00:23:49 +0200

    Update Axion_coannihilation.cpp

--- processes/Axion_coannihilation.cpp
+++ processes/Axion_coannihilation.cpp
@@ -1,16 +1,18 @@
+// Example process file for the annihilation of muons into photon and axion
 #include "Model.h"
 
 // **********************  Process file  **********************
 
-// Constant values of masses (MAYBE TRANSFERRED TO A DIFFERENT FILE?)
+// Muon mass
 const double Mmu = 0.1056; // [GeV]
-const double MPhoton = 0.00001; // [GeV]
+// Photon mass
+const double MPhoton = 0.00001; // [GeV] non-zero mass to avoid potential errors
 
 // Masses of the particles
 double Mi = Mmu;
 double Mj = Mmu;
 double Mk = MPhoton;
-double Mx = 0.00001; // DM particle mass [GeV]
+double Mx = 0.00001; // axion mass [GeV]
 
 // Types of particles
 char s_i = 'f';
@@ -20,7 +22,7 @@ char s_k = 'b';
 // Name of the process
 std::string pr_name = "Ann";
 
-// Amplitude squared 	(without the constant part in front)
+// Amplitude squared (without the constant part in front)
 double M2(double s, double t)
 {
 	return s*s/(Mmu*Mmu - t)/(s + t - Mmu*Mmu); // [1]
