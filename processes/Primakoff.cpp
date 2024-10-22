9094e3d70827a747c03f81d66059decf110517af
Maxim Laletin <maxim.n.laletin@gmail.com>
2024-10-23 00:18:29 +0200

    Update Primakoff.cpp

--- processes/Primakoff.cpp
+++ processes/Primakoff.cpp
@@ -1,10 +1,10 @@
+// Example process file for the Primakoff scattering of axion on muon
 #include <string>
 #include "Model.h"
 
 const double Mmu = 0.1056;
-const double MPhoton = 0.00000001; 
+const double MPhoton = 0.00000001; // non-zero mass to avoid potential errors
 
-// The user definitely needs to provide these four masses
 double Mi = Mmu;
 double Mj = MPhoton;
 double Mk = Mmu;
@@ -14,6 +14,7 @@ char s_i = 'f';
 char s_j = 'b';
 char s_k = 'f';
 
+// Process name used to create the file with the results
 std::string pr_name = "Prim";
 
 // Amplitude squared (without the constant part in front)
